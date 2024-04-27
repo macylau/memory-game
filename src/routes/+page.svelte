21d4eae25552c7d1284fb27d531004943e5e4116
Macy Lau <macylau94@gmail.com>
2024-04-27 17:25:40 -0600

    added emoji

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,30 @@
-<h1 class="text-3xl text-center mt-16">Memory Game</h1>
-<div class="container bg-indigo-400/50 flex justify-center my-8 mx-auto p-8 min-w-3xl max-w-4xl shadow-lg rounded-3xl">
-	<div class="card"></div>
-</div>
-<!-- <button type="button" class="btn variant-filled-primary flex justify-center ">Restart</button> -->
+<script lang="ts">
+	import { emoji } from './emoji'
 
+	type State = 'start' | 'playing' | 'paused' | 'won' | 'lost'
+
+	let state: State = 'start'
+	let size = 20
+	let grid = createGrid()
+
+	function createGrid () {
+		let cards = new Set<string>()
+		let maxSize = size / 2
+
+		while (cards.size < maxSize) {
+			const randomIndex = Math.floor(Math.random() * emoji.length)
+			cards.add(emoji[randomIndex])
+		}
+
+		return shuffle([...cards, ...cards])
+	}
+
+	function shuffle<Items>(array: Items[]) {
+		return array.sort(() => Math.random() - 0.5)
+	}
+	console.log(grid)
+</script>
+
+<div>
+	<h1 class="text-3xl text-center mt-16">Memory Game</h1>
+</div>
