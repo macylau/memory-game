49e994837e43ef52813a268e4b76e464c890ce69
Macy Lau <macylau94@gmail.com>
2024-04-27 18:29:23 -0600

    working emoji on game board

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,16 +4,5 @@
 
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<a href="/" class="text-xl font-bold">Gotta Flip'em All!</a>
-			</svelte:fragment>
-			
-		</AppBar>
-	</svelte:fragment>
 	<slot />
-</AppShell>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,11 @@
 	let state: State = 'start'
 	let size = 20
 	let grid = createGrid()
+	let maxMatches = grid.length / 2
+	let seleted: number[] = []
+	let matches: string[] = []
 
-	function createGrid () {
+	function createGrid() {
 		let cards = new Set<string>()
 		let maxSize = size / 2
 
@@ -25,6 +28,28 @@
 	console.log(grid)
 </script>
 
-<div>
-	<h1 class="text-3xl text-center mt-16">Memory Game</h1>
-</div>
+<h1 class="text-3xl text-center mt-14">Memory Game</h1>
+{#if state == 'start'}
+	<button class="btn btn-xl variant-outline-primary hover:variant-filled-primary flex justify-center mx-auto my-8 font-bold" on:click={() => (state = 'playing')}>Start</button>
+{/if}
+
+{#if state == 'playing'}
+	<div class="max-w-md mx-auto cards grid grid-cols-5 gap-4 justify-items-center m-auto overflow-hidden md:max-w-3xl p-8">
+		{#each grid as card, cardIndex}
+			<button class="card drop-shadow-lg w-32 h-32 rounded-lg focus:border-4 border-pink-400">
+				<div class="text-6xl">{card}</div>
+			</button>
+		{/each}
+	</div>
+{/if}
+
+{#if state == 'lost'}
+	<h1>Game Over! 🥺 </h1>
+	<button on:click={() => state = 'playing'}>Play again</button>
+{/if}
+
+{#if state == 'won'}
+	<h1>Victory is yours! You've won! 🥳</h1>
+	<button on:click={() => state = 'playing'}>Play again</button>
+{/if}
+
